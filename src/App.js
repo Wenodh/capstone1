a88d2ca6df9d50a2827ee4d08e643e5e57af5c6d
Alurivaraprasad <aluri_varaprasad@softprodigy.com>
2023-09-10 20:21:55 +0530

    added Og and semantic tags

--- src/App.js
+++ src/App.js
@@ -1,11 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+
   );
 }
 
 export default App;
+function Main() {
+  return <main>
+    main
+  </main>;
+}
+
+function Footer() {
+  return <footer>
+    footer
+  </footer>;
+}
+
+function Header() {
+  return <header>
+    <img alt="logo" />
+    <Nav />
+  </header>;
+}
+
+function Nav() {
+  return <nav>
+    <ul>
+      <li><a href="/home">Homepage</a></li>
+      <li><a href="/about">About Us</a></li>
+      <li><a href="/blog">Blog</a></li>
+    </ul>
+  </nav>;
+}
+
